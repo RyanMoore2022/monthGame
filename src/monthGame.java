e7d56c6c4f5e9c5b312f583f8c89adf57837501f
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-11-11 09:21:43 -0500

    month calc

--- src/monthGame.java
+++ src/monthGame.java
@@ -1,8 +1,28 @@
 
 public class monthGame {
     public static void main(String[] args) {
-
+        Zellercongruence(22, 10, 2017);
 
     }
+    // Print Day for a Date
+    static void Zellercongruence(int day, int month, int year)
+    {
+        if (month == 1)
+        {
+            month = 13;
+            year--;
+        }
+        if (month == 2)
+        {
+            month = 14;
+            year--;
+        }
+
+        int q = day;
+        int m = month;
+        int k = year % 100;
+        int j = year / 100;    }
 
 }
+
+
