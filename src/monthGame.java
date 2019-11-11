b7a2583def2041a8daccef1cc31a1262b8717c52
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-11-11 09:40:03 -0500

    algorithm added

--- src/monthGame.java
+++ src/monthGame.java
@@ -4,24 +4,41 @@ public class monthGame {
         Zellercongruence(22, 10, 2017);
 
     }
-    // Print Day for a Date
-    static void Zellercongruence(int day, int month, int year)
-    {
-        if (month == 1)
-        {
-            month = 13;
-            year--;
-        }
-        if (month == 2)
-        {
-            month = 14;
-            year--;
-        }
+    static void Zellercongruence(int day, int month, int year) {
 
         int q = day;
         int m = month;
         int k = year % 100;
-        int j = year / 100;    }
+        int j = year / 100;
+        int h = q + 13*(m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+        h = h % 7;
+
+        switch (month) {
+            case 1:
+                (month == 1){
+                month = 13;
+                year--;
+                }
+                break;
+            case 2:
+                (month == 2){
+                month = 14;
+                year--;
+
+            }
+            default:{
+                System.out.println("can't compute.");
+            }
+        }
+
+
+
+
+
+
+
+
+    }
 
 }
 
